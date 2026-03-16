8b48f175bd62d32960e5e2af97321d46ba52fc0a
Wiktor Sołtys <wiesiekdx@gmail.com>
2026-03-16 14:10:22 +0100

    Feat(SNTP client): add time-sync via SNTP

--- main/main.c
+++ main/main.c
@@ -1,5 +1,11 @@
+#include <time.h>
+
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
 #include <esp_err.h>
 #include <esp_log.h>
+#include <esp_netif_sntp.h>
 #include <nvs_flash.h>
 
 #include <sdkconfig.h>
@@ -8,6 +14,16 @@ static const char* TAG = "main";
 
 esp_err_t connect_to_wifi(const char* ssid, const char* password, int retry_num);
 esp_err_t ledriver_ota_check_and_update(void);
+
+esp_err_t init_sntp(size_t timeout_ms) {
+    setenv("TZ", "CET-1CEST,M3.5.0/2,M10.5.0/3", 1);
+    tzset();
+
+    esp_sntp_config_t config = ESP_NETIF_SNTP_DEFAULT_CONFIG("pool.ntp.org");
+    esp_netif_sntp_init(&config);
+
+    return esp_netif_sntp_sync_wait(pdMS_TO_TICKS(timeout_ms));
+}
 
 void app_main(void) {
     esp_err_t result = nvs_flash_init();
@@ -20,6 +36,8 @@ void app_main(void) {
     result = connect_to_wifi(CONFIG_APP_WIFI_SSID, CONFIG_APP_WIFI_PASSWORD, 2);
     if (result != ESP_OK)
         ESP_LOGE(TAG, "connect_to_wifi: %s", esp_err_to_name(result));
+
+    init_sntp(10000);
 
     if (result == ESP_OK) {
         result = ledriver_ota_check_and_update();
