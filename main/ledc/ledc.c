d475f52193d36cab7482262222b95afd52835d5b
Wiktor Sołtys <wiesiekdx@gmail.com>
2026-04-18 21:26:28 +0200

    Feat(LEDC): add power switching

--- main/ledc/ledc.c
+++ main/ledc/ledc.c
@@ -12,6 +12,7 @@
 #define LEDC_CHANNEL_BLUE LEDC_CHANNEL_2
 
 static ledriver_ledc_brightness_state_t current_state = {};
+static bool power_on = false;
 
 esp_err_t ledriver_ledc_init(int r_gpio, int g_gpio, int b_gpio) {
     const ledc_timer_config_t timer = {.speed_mode = LEDC_LOW_SPEED_MODE,
@@ -61,58 +62,72 @@ esp_err_t ledriver_ledc_init(int r_gpio, int g_gpio, int b_gpio) {
     return ESP_OK;
 }
 
-esp_err_t ledriver_ledc_set_red(uint16_t brightness) {
-    esp_err_t result = ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_RED, brightness);
-    if (result != ESP_OK)
-        return result;
+static esp_err_t ledc_set_brightness(ledc_channel_t channel, uint16_t brightness, bool save,
+                                     bool force) {
+    if (channel != LEDC_CHANNEL_RED && channel != LEDC_CHANNEL_GREEN &&
+        channel != LEDC_CHANNEL_BLUE)
+        return ESP_ERR_INVALID_ARG;
 
-    result = ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_RED);
-    if (result != ESP_OK)
-        return result;
+    if (power_on || force) {
+        esp_err_t result = ledc_set_duty(LEDC_LOW_SPEED_MODE, channel, brightness);
+        if (result != ESP_OK)
+            return result;
 
-    current_state.r = brightness;
+        result = ledc_update_duty(LEDC_LOW_SPEED_MODE, channel);
+        if (result != ESP_OK)
+            return result;
+    }
+
+    if (save) {
+        switch (channel) {
+        case LEDC_CHANNEL_RED: {
+            current_state.r = brightness;
+            break;
+        }
+
+        case LEDC_CHANNEL_GREEN: {
+            current_state.g = brightness;
+            break;
+        }
+
+        case LEDC_CHANNEL_BLUE: {
+            current_state.b = brightness;
+            break;
+        }
+
+        default:
+            return ESP_ERR_INVALID_ARG;
+        }
+    }
     return ESP_OK;
 }
 
-esp_err_t ledriver_ledc_set_green(uint16_t brightness) {
-    esp_err_t result = ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_GREEN, brightness);
-    if (result != ESP_OK)
-        return result;
-
-    result = ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_GREEN);
-    if (result != ESP_OK)
-        return result;
-
-    current_state.g = brightness;
-    return ESP_OK;
+static esp_err_t ledc_set_red(uint16_t brightness, bool save, bool force) {
+    return ledc_set_brightness(LEDC_CHANNEL_RED, brightness, save, force);
 }
 
-esp_err_t ledriver_ledc_set_blue(uint16_t brightness) {
-    esp_err_t result = ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_BLUE, brightness);
-    if (result != ESP_OK)
-        return result;
-
-    result = ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_BLUE);
-    if (result != ESP_OK)
-        return result;
-
-    current_state.b = brightness;
-    return ESP_OK;
+static esp_err_t ledc_set_green(uint16_t brightness, bool save, bool force) {
+    return ledc_set_brightness(LEDC_CHANNEL_GREEN, brightness, save, force);
 }
 
-esp_err_t ledriver_ledc_set_rgb(const ledriver_ledc_brightness_state_t* state) {
+static esp_err_t ledc_set_blue(uint16_t brightness, bool save, bool force) {
+    return ledc_set_brightness(LEDC_CHANNEL_BLUE, brightness, save, force);
+}
+
+static esp_err_t ledc_set_rgb(const ledriver_ledc_brightness_state_t* state, bool save,
+                              bool force) {
     if (state == NULL)
         return ESP_ERR_INVALID_ARG;
 
-    esp_err_t result = ledriver_ledc_set_red(state->r);
+    esp_err_t result = ledc_set_red(state->r, save, force);
     if (result != ESP_OK)
         return result;
 
-    result = ledriver_ledc_set_green(state->g);
+    result = ledc_set_green(state->g, save, force);
     if (result != ESP_OK)
         return result;
 
-    result = ledriver_ledc_set_blue(state->b);
+    result = ledc_set_blue(state->b, save, force);
     if (result != ESP_OK)
         return result;
 
@@ -125,4 +140,44 @@ esp_err_t ledriver_ledc_get_rgb(ledriver_ledc_brightness_state_t* state) {
 
     *state = current_state;
     return ESP_OK;
+}
+
+esp_err_t ledriver_ledc_set_power(bool power) {
+    if (power) {
+        const esp_err_t result = ledc_set_rgb(&current_state, false, true);
+        if (result != ESP_OK)
+            return result;
+    } else {
+        static const ledriver_ledc_brightness_state_t power_off_state = {0};
+
+        const esp_err_t result = ledc_set_rgb(&power_off_state, false, true);
+        if (result != ESP_OK)
+            return result;
+    }
+    power_on = power;
+    return ESP_OK;
+}
+
+esp_err_t ledriver_ledc_get_power(bool* power) {
+    if (power == NULL)
+        return ESP_ERR_INVALID_ARG;
+
+    *power = power_on;
+    return ESP_OK;
+}
+
+esp_err_t ledriver_ledc_set_red(uint16_t brightness) {
+    return ledc_set_brightness(LEDC_CHANNEL_RED, brightness, true, false);
+}
+
+esp_err_t ledriver_ledc_set_green(uint16_t brightness) {
+    return ledc_set_brightness(LEDC_CHANNEL_GREEN, brightness, true, false);
+}
+
+esp_err_t ledriver_ledc_set_blue(uint16_t brightness) {
+    return ledc_set_brightness(LEDC_CHANNEL_BLUE, brightness, true, false);
+}
+
+esp_err_t ledriver_ledc_set_rgb(const ledriver_ledc_brightness_state_t* state) {
+    return ledc_set_rgb(state, true, false);
 }
--- main/ledc/ledc.h
+++ main/ledc/ledc.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <stdint.h>
+#include <stdbool.h>
 
 #include <esp_err.h>
 
@@ -16,3 +17,6 @@ esp_err_t ledriver_ledc_set_blue(uint16_t brightness);
 
 esp_err_t ledriver_ledc_set_rgb(const ledriver_ledc_brightness_state_t* state);
 esp_err_t ledriver_ledc_get_rgb(ledriver_ledc_brightness_state_t* state);
+
+esp_err_t ledriver_ledc_set_power(bool power);
+esp_err_t ledriver_ledc_get_power(bool* power);
