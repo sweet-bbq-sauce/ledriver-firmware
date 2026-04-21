abf3b39f02a2ee084190bbeb37dbb5af08c8e3a0
Wiktor Sołtys <wiesiekdx@gmail.com>
2026-04-21 15:51:39 +0200

    Fix(main): init LEDC before HTTPD

--- main/main.c
+++ main/main.c
@@ -4,8 +4,8 @@
 
 #include <sdkconfig.h>
 
-#include <webpanel/partition.h>
 #include <ledc/ledc.h>
+#include <webpanel/partition.h>
 
 static const char* TAG = "main";
 
@@ -20,6 +20,10 @@ void app_main(void) {
         result = nvs_flash_init();
     }
     ESP_ERROR_CHECK(result);
+
+    result = ledriver_ledc_init(25, 26, 27);
+    if (result != ESP_OK)
+        ESP_LOGE(TAG, "LEDC init error: %s", esp_err_to_name(result));
 
     result = connect_to_wifi(CONFIG_APP_WIFI_SSID, CONFIG_APP_WIFI_PASSWORD, 2);
     if (result != ESP_OK)
@@ -38,8 +42,4 @@ void app_main(void) {
     result = ledriver_start_webpanel_server();
     if (result != ESP_OK)
         ESP_LOGE(TAG, "httpd server error: %s", esp_err_to_name(result));
-
-    result = ledriver_ledc_init(25, 26, 27);
-    if (result != ESP_OK)
-        ESP_LOGE(TAG, "LEDC init error: %s", esp_err_to_name(result));
 }
